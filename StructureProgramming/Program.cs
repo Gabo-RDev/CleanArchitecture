090f302b0b91df60c96ee3a0f761739688f6cd82
ImNotGabo <gabriel.duarte18@outlook.es>
2025-02-03 19:27:00 -0400

    Variables in C#

--- StructureProgramming/Program.cs
+++ StructureProgramming/Program.cs
@@ -4,4 +4,18 @@ Console.WriteLine("Hello, World!");
 
 int number = 12;
 number = 20;
+float deci = 12.3f; // hasta 7 digitos
 double dec = 3.14;
+
+// Booleanos
+
+bool isThereBeer = true;
+
+// Strings
+string name = "Gabriel";
+char land = 'L'; // para guardar letras
+
+// Declarar variables
+var num = 15; // Con var se infiere el tipo
+var firstName = "Wilmer";
+//name = "Dayron"; // No se puede re-declarar
