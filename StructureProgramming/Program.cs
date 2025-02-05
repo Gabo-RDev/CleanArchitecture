8850d8de5c2640692d589189b34a64136fcaaf3b
ImNotGabo <gabriel.duarte18@outlook.es>
2025-02-05 19:57:19 -0400

    Ejemplo de programa con paradigma estructurado

--- StructureProgramming/Program.cs
+++ StructureProgramming/Program.cs
@@ -117,3 +117,62 @@ void Bye()
 {
     Show("Adios");
 }
+
+// Ejemplo
+int limit  = 10;
+var beers = new string[limit];
+int iBeers = 0;
+int op  = 0;
+
+do 
+{
+    Console.Clear();
+    ShowMneu();
+    op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            if (iBeers < limit)
+            {
+                Console.Clear();
+                Console.WriteLine("Escribe nombre de la cerveza");
+                var beer = Console.ReadLine();
+                beers[iBeers] = beer;
+                iBeers++;
+            } else
+            {
+                Console.WriteLine("Ya no caben cervezas");
+            }
+            break;
+        case 2:
+            Console.Clear();
+            ShowBeers(beers, iBeers);
+            break;  
+        case 3:
+            Console.WriteLine("Adios");
+			break;
+        default:
+            Console.WriteLine("Opcion no valida");
+            break;
+    }
+
+} while(op != 3);
+
+void ShowMneu()
+{
+    Console.WriteLine("1. Agregar nombre");
+    Console.WriteLine("2. Mostrar nombre");
+    Console.WriteLine("3. Salir");
+}
+
+void ShowBeers(string[] beers, int iBeers)
+{
+    Console.Clear();
+    Console.WriteLine("----Cerverzas-----");
+	for (int i = 0; i <= iBeers; i++)
+	{
+        Console.WriteLine(beers[i]);
+	}
+    Console.WriteLine("Presiona una tecla para continuar");
+    Console.ReadLine();
+}
