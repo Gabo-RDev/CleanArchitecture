cedc7c9966a1575017f8f43836c3eb403146ca34
ImNotGabo <gabriel.duarte18@outlook.es>
2025-02-08 17:14:47 -0400

    Namespace

---
+++ OOP/Business/Beer.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP.Business
+{
+	public class Beer
+	{
+
+		private decimal _alcohol; // campo
+		public string Name { get; set; }
+
+		public decimal Price { get; set; } // Propiedad automatica
+
+		public decimal Alcohol // Propiedad
+		{
+			get { return _alcohol; }
+			set
+			{
+				if (value < 0)
+				{
+					value = 0;
+				}
+			}
+		}
+
+		public string SAlcohol
+		{
+			get
+			{
+				return "Alcohol: " + _alcohol.ToString();
+			}
+		}
+
+		public Beer(string name, decimal price, decimal alcohol)
+		{
+			Name = name;
+			Price = price;
+			Alcohol = alcohol;
+		}
+
+		public string GetInfo()
+		{
+			return "Nombre: " + Name + ", Precio: $" + Price + ", Alcohol " + Alcohol;
+		}
+	}
+}
+
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,45 +1,6 @@
-﻿Beer presidente = new Beer("Presidente", 3, -2);
+﻿using OOP.Business;
+
+Beer presidente = new Beer("Presidente", 3, -2);
 
 Console.WriteLine(presidente.Name);
 Console.WriteLine(presidente.SAlcohol);
-
-
-public class Beer
-{
-	private decimal _alcohol; // campo
-	public string Name { get; set; }
-
-	public decimal Price { get; set; } // Propiedad automatica
-
-	public decimal Alcohol // Propiedad
-	{
-		get { return _alcohol; }
-		set
-		{
-			if (value < 0)
-			{
-				value = 0;
-			}
-		}
-	}
-
-	public string SAlcohol
-	{
-		get
-		{
-			return "Alcohol: " + _alcohol.ToString();
-		}
-	}
-
-	public Beer(string name, decimal price, decimal alcohol)
-	{
-		Name = name;
-		Price = price;
-		Alcohol = alcohol;
-	}
-
-	public string GetInfo()
-	{
-		return "Nombre: " + Name + ", Precio: $" + Price + ", Alcohol " + Alcohol;
-	}
-}
